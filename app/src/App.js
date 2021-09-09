778c798b83bcd586202d09462638dd0415fa2319
Huimin <huiminzhang0111@hotmail.com>
2021-09-08 21:22:51 -0700

    sport app

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Sport from './components/Sport';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Random Sport App</h1>
+      <Sport />
+    </div>
+  );
+}
+
+export default App;
